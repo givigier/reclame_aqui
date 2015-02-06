29d468ce7d5ffbf5bdad6965f31325dfd332d1b4
Oswaldo S Ferreira <oswaldo.ferreira@iff.edu.br>
2015-02-05 22:10:36 -0200

    just adding some simple perks

--- spec/reclame_aqui/reputation_by_website_spec.rb
+++ spec/reclame_aqui/reputation_by_website_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 describe ReclameAqui::ReputationByWebsite do
   context "when passing an invalid website" do
     let!(:reclame_aqui){ ReclameAqui::ReputationByWebsite.new("http://").get_reputation }
-    let!(:error){ reclame_aqui[:error] }
-    let!(:reputation){ reclame_aqui[:reputation] }
+    let!(:error){ reclame_aqui.error }
+    let!(:reputation){ reclame_aqui.reputation }
 
     it "should return a hash with error equal true" do
       expect(error).to eq true
@@ -17,71 +17,71 @@ describe ReclameAqui::ReputationByWebsite do
 
   context "when passing a valid website" do
     let!(:reclame_aqui){ ReclameAqui::ReputationByWebsite.new("http://ricardoeletro.com.br").get_reputation }
-    let!(:error){ reclame_aqui[:error] }
-    let!(:reputation){ reclame_aqui[:reputation] }
+    let!(:error){ reclame_aqui.error }
+    let!(:reputation){ reclame_aqui.reputation }
 
     it "should return a hash with error equal false" do
       expect(error).to eq false
     end
 
     it "should return the reputation with id" do
-      expect(reputation[:id]).to eq 10708
+      expect(reputation.id).to eq 10708
     end
 
     it "should return the reputation with name" do
-      expect(reputation[:name]).to eq "Ricardo Eletro ( Internet )"
+      expect(reputation.name).to eq "Ricardo Eletro ( Internet )"
     end
 
     it "should return the reputation with period" do
-      expect(reputation[:period]).to_not eq nil
+      expect(reputation.period).to_not be_nil
     end
 
     it "should return the reputation with status" do
-      expect(reputation[:status]).to_not eq nil
+      expect(reputation.status).to_not be_nil
     end
 
     it "should return the reputation with complaints" do
-      expect(reputation[:complaints]).to_not eq nil
+      expect(reputation.complaints).to_not be_nil
     end
 
     it "should return the reputation with answered" do
-      expect(reputation[:answered]).to_not eq nil
+      expect(reputation.answered).to_not be_nil
     end
 
     it "should return the reputation with not_answered" do
-      expect(reputation[:not_answered]).to_not eq nil
+      expect(reputation.not_answered).to_not be_nil
     end
 
     it "should return the reputation with measured" do
-      expect(reputation[:measured]).to_not eq nil
+      expect(reputation.measured).to_not be_nil
     end
 
     it "should return the reputation with solution_index" do
-      expect(reputation[:solution_index]).to_not eq nil
+      expect(reputation.solution_index).to_not be_nil
     end
 
     it "should return the reputation with would_return" do
-      expect(reputation[:would_return]).to_not eq nil
+      expect(reputation.would_return).to_not be_nil
     end
 
     it "should return the reputation with grade" do
-      expect(reputation[:grade]).to_not eq nil
+      expect(reputation.grade).to_not be_nil
     end
 
     it "should return the reputation with time_to_response" do
-      expect(reputation[:time_to_response]).to_not eq nil
+      expect(reputation.time_to_response).to_not be_nil
     end
 
     it "should return the reputation with chat" do
-      expect(reputation[:chat]).to_not eq nil
+      expect(reputation.chat).to_not be_nil
     end
 
     it "should return the reputation with fone" do
-      expect(reputation[:fone]).to_not eq nil
+      expect(reputation.fone).to_not be_nil
     end
 
     it "should return the reputation with show_selo" do
-      expect(reputation[:show_selo]).to_not eq nil
+      expect(reputation.show_selo).to_not be_nil
     end
   end
 end
