1b75a7d8f8152ff5e33f5b326d0b8ca6de4245bb
Gabriel Givigier <gabriel@givigier.com>
2018-08-10 00:35:23 -0300

    Update code style of reputation_by_website

--- lib/reclame_aqui/reputation_by_website.rb
+++ lib/reclame_aqui/reputation_by_website.rb
@@ -1,47 +1,53 @@
-require "json"
+# frozen_string_literal: true
 
-class ReclameAqui::ReputationByWebsite
-  def initialize(website)
-    @website = website
-  end
+require 'json'
 
-  def get_reputation
-    begin
-      response = do_request
-      format_response JSON.parse(response.body)
-    rescue
-      { error: true, reputation: {} }
+module ReclameAqui
+  class ReputationByWebsite
+    def initialize(website)
+      @website = website
     end
-  end
 
-  private
-  def do_request
-    uri = URI.parse "http://app02.reclameaqui.com.br/reputacao"
-    http = Net::HTTP.new uri.host, uri.port
-    response = http.post uri.path, "url=#{@website}", {}
-  end
+    def get_reputation
+      begin
+        response = do_request
+        format_response JSON.parse(response.body)
+      rescue
+        { error: true, reputation: {} }
+      end
+    end
 
-  def format_response data
-    reputation = data["reputacao"]
-    status = reputation["status"] || []
-    { error: false,
-      reputation: {
-        id: reputation["empresa_id"].to_i, 
-        name: reputation["nome_empresa"], 
-        period: reputation["periodo"].to_i, 
-        status: status["status_id"].to_i, 
-        complaints: reputation["reclamacoes"].to_i,
-        answered: reputation["respondidas"].to_i, 
-        not_answered: reputation["nao_respondidas"].to_i, 
-        measured: reputation["avaliadas"].to_i, 
-        solution_index: reputation["indice_solucao"].to_i,
-        would_return: reputation["voltaria_fazer_negocio"].to_i,
-        grade: reputation["nota_consumidor"].to_i,
-        time_to_response: reputation["tempo_resposta"],
-        chat: reputation["chat"].to_i,
-        fone: reputation["fone"].to_i,
-        show_selo: reputation["show_selo"].to_i
+    private
+
+    def do_request
+      uri = URI.parse 'http://app02.reclameaqui.com.br/reputacao'
+      http = Net::HTTP.new uri.host, uri.port
+      http.post uri.path, "url=#{@website}", {}
+    end
+
+    def format_response data
+      reputation = data['reputacao']
+      status = reputation['status'] || []
+      {
+        error: false,
+        reputation: {
+          id: reputation['empresa_id'].to_i,
+          name: reputation['nome_empresa'],
+          period: reputation['periodo'].to_i,
+          status: status['status_id'].to_i,
+          complaints: reputation['reclamacoes'].to_i,
+          answered: reputation['respondidas'].to_i,
+          not_answered: reputation['nao_respondidas'].to_i,
+          measured: reputation['avaliadas'].to_i,
+          solution_index: reputation['indice_solucao'].to_i,
+          would_return: reputation['voltaria_fazer_negocio'].to_i,
+          grade: reputation['nota_consumidor'].to_i,
+          time_to_response: reputation['tempo_resposta'],
+          chat: reputation['chat'].to_i,
+          fone: reputation['fone'].to_i,
+          show_selo: reputation['show_selo'].to_i
+        }
       }
-    }
+    end
   end
 end
--- spec/reclame_aqui/reputation_by_website_spec.rb
+++ spec/reclame_aqui/reputation_by_website_spec.rb
@@ -1,86 +1,93 @@
-require "spec_helper"
+# frozen_string_literal: true
+
+require 'spec_helper'
 
 describe ReclameAqui::ReputationByWebsite do
-  context "when passing an invalid website" do
-    let!(:reclame_aqui){ ReclameAqui::ReputationByWebsite.new("http://").get_reputation }
-    let!(:error){ reclame_aqui[:error] }
-    let!(:reputation){ reclame_aqui[:reputation] }
+  context 'when passing an invalid website' do
+    let!(:reclame_aqui) do
+      ReclameAqui::ReputationByWebsite.new('http://').get_reputation
+    end
+    let!(:error) { reclame_aqui[:error] }
+    let!(:reputation) { reclame_aqui[:reputation] }
 
-    it "should return a hash with error equal true" do
+    it 'should return a hash with error equal true' do
       expect(error).to eq true
     end
 
-    it "should return an empty reputation" do
+    it 'should return an empty reputation' do
       expect(reputation).to eq Hash.new({})
     end
   end
 
-  context "when passing a valid website" do
-    let!(:reclame_aqui){ ReclameAqui::ReputationByWebsite.new("http://ricardoeletro.com.br").get_reputation }
-    let!(:error){ reclame_aqui[:error] }
-    let!(:reputation){ reclame_aqui[:reputation] }
+  context 'when passing a valid website' do
+    let!(:reclame_aqui) do
+      ReclameAqui::ReputationByWebsite
+        .new('http://ricardoeletro.com.br').get_reputation
+    end
+    let!(:error) { reclame_aqui[:error] }
+    let!(:reputation) { reclame_aqui[:reputation] }
 
-    it "should return a hash with error equal false" do
+    it 'should return a hash with error equal false' do
       expect(error).to eq false
     end
 
-    it "should return the reputation with id" do
+    it 'should return the reputation with id' do
       expect(reputation[:id]).to eq 10708
     end
 
-    it "should return the reputation with name" do
-      expect(reputation[:name]).to eq "Ricardo Eletro  - Loja Online"
+    it 'should return the reputation with name' do
+      expect(reputation[:name]).to eq 'Ricardo Eletro  - Loja Online'
     end
 
-    it "should return the reputation with period" do
+    it 'should return the reputation with period' do
       expect(reputation[:period]).to_not eq nil
     end
 
-    it "should return the reputation with status" do
+    it 'should return the reputation with status' do
       expect(reputation[:status]).to_not eq nil
     end
 
-    it "should return the reputation with complaints" do
+    it 'should return the reputation with complaints' do
       expect(reputation[:complaints]).to_not eq nil
     end
 
-    it "should return the reputation with answered" do
+    it 'should return the reputation with answered' do
       expect(reputation[:answered]).to_not eq nil
     end
 
-    it "should return the reputation with not_answered" do
+    it 'should return the reputation with not_answered' do
       expect(reputation[:not_answered]).to_not eq nil
     end
 
-    it "should return the reputation with measured" do
+    it 'should return the reputation with measured' do
       expect(reputation[:measured]).to_not eq nil
     end
 
-    it "should return the reputation with solution_index" do
+    it 'should return the reputation with solution_index' do
       expect(reputation[:solution_index]).to_not eq nil
     end
 
-    it "should return the reputation with would_return" do
+    it 'should return the reputation with would_return' do
       expect(reputation[:would_return]).to_not eq nil
     end
 
-    it "should return the reputation with grade" do
+    it 'should return the reputation with grade' do
       expect(reputation[:grade]).to_not eq nil
     end
 
-    it "should return the reputation with time_to_response" do
+    it 'should return the reputation with time_to_response' do
       expect(reputation[:time_to_response]).to_not eq nil
     end
 
-    it "should return the reputation with chat" do
+    it 'should return the reputation with chat' do
       expect(reputation[:chat]).to_not eq nil
     end
 
-    it "should return the reputation with fone" do
+    it 'should return the reputation with fone' do
       expect(reputation[:fone]).to_not eq nil
     end
 
-    it "should return the reputation with show_selo" do
+    it 'should return the reputation with show_selo' do
       expect(reputation[:show_selo]).to_not eq nil
     end
   end
