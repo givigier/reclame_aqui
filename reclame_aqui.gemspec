034b5e0de2a59f137252ae9510927ec75f10ccd0
Gabriel Givigier <gabriel@givigier.com>
2018-10-28 11:20:37 -0300

    Add simplecov to measure the code coverage

--- reclame_aqui.gemspec
+++ reclame_aqui.gemspec
@@ -24,4 +24,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency 'bundler', '~> 1.16.3'
   spec.add_development_dependency 'rspec', '~> 3.8.0'
+  spec.add_development_dependency 'simplecov', '~> 0.16.1'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "reclame_aqui"
+require "simplecov"
 
 RSpec.configure do |config|
+  SimpleCov.start
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
